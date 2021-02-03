fcf433f281dff9c7b131116786787c8db54f89d1
Zachary Voo <zackryanv@hotmail.com>
2021-02-03 15:31:08 -0700

    practiced java

--- cc-events/dd-image-description-ex/js/main.js
+++ cc-events/dd-image-description-ex/js/main.js
@@ -1,1 +1,12 @@
 //Put your solution in this file
+var featureLink = document.querySelector('p.city-intro a');
+function featureLinkHandler (evt)
+{
+    let featureImage = document.querySelector('img.feature')
+    featureImage.src = featureLink.href;
+    featureImage.classList.remove('hidden');
+
+    evt.preventDefault();
+}
+featureLink,addEventListener('click', featureLinkHandler)   
+
