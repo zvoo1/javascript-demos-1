bb69014ce9621152fe5049be1df39b9369495ebe
Zachary Voo <zackryanv@hotmail.com>
2021-03-04 18:29:51 -0700

    did some work

--- ee-forms/ee-exercise/js/main.js
+++ ee-forms/ee-exercise/js/main.js
@@ -4,3 +4,28 @@
 // (i.e. cannot be whitespace). If the user tries to add an empty tag, remove the hidden 
 // class from the p.feature.error element.  If the form is submitted correctly, then ensure
 // that the p.feature.error element is hidden.
+document.querySelector('.feature.frm ')
+        .addEventListener('submit', function (evt) 
+        {
+          let error = document.querySelector('p.feature.error');
+          let textBox = evt.target.elements.tag;
+
+          if (textBox.value.trim() === '') 
+          {
+              error.innerHTML = 'Tags cannot be empty';
+              error.classList.remove('hidden');
+          } 
+          else if(textBox.value.indexOf(' ') >= 0)
+          {
+            error.innerHTML = 'Tags cannot contain spaces';
+            error.classList.remove('hidden');
+          }
+          else 
+          {
+              document.querySelector('p.feature.tags ')
+              .innerHTML += ' #' + textBox.value.trim();
+              textBox.value = '';
+              error.classList.add('hidden');
+          }
+          evt.preventDefault();
+        })
